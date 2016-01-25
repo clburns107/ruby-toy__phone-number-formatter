262fd2ba58051e255ec27b6de69d446a3643702a
Courtney Burns <courtneylynnburns@gmail.com>
2016-01-25 17:39:08 -0600

    fin

--- lib/phone_number_formatter.rb
+++ lib/phone_number_formatter.rb
@@ -2,11 +2,10 @@
 # returns a properly formatted phone number.
 
 def format_phone_number(phone_number_str)
-  arr=phone_number_str.split('')
+  arr=phone_number_str.gsub(/[(). -]/, "").split('')
   arr.insert(0,"(")
   arr.insert(4,")")
   arr.insert(5," ")
   arr.insert(9,"-")
   puts arr.join
 end
-format_phone_number("4023018452")
